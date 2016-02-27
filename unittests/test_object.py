82b640b2f755a527b45bb3378dc12ccee0154a11
mingzai.ym <mingzai.ym@alibaba-inc.com>
2016-02-27 13:56:14 +0800

    change more cases

--- unittests/test_object.py
+++ unittests/test_object.py
@@ -43,68 +43,183 @@ def r4append(next_position, in_status=200, in_headers=None):
 class TestObject(OssTestCase):
     @patch('oss2.Session.do_request')
     def test_head(self, do_request):
-        size = 1024
-        resp = r4head(size)
-        do_request.return_value = resp
+        request_text = '''HEAD /apbmntxqtvxjzini HTTP/1.1
+Host: ming-oss-share.oss-cn-hangzhou.aliyuncs.com
+Accept-Encoding: identity
+Connection: keep-alive
+date: Sat, 12 Dec 2015 00:35:55 GMT
+User-Agent: aliyun-sdk-python/2.0.2(Windows/7/;3.3.3)
+Accept: */*
+authorization: OSS ZCDmm7TPZKHtx77j:Q05CWxpclrtNnUWHY5wS10fhFk0='''
 
-        result = bucket().head_object('fake-key')
+        response_text = '''HTTP/1.1 200 OK
+Server: AliyunOSS
+Date: Sat, 12 Dec 2015 00:35:55 GMT
+Content-Type: application/octet-stream
+Content-Length: 10
+Connection: keep-alive
+x-oss-request-id: 566B6BEBD4C05B21E97261B0
+Accept-Ranges: bytes
+ETag: "0CF031A5EB9351746195B20B86FD3F68"
+Last-Modified: Sat, 12 Dec 2015 00:35:54 GMT
+x-oss-object-type: Normal'''
 
-        self.assertEqual(result.content_length, size)
+        req_info = mock_response(do_request, response_text)
+
+        result = bucket().head_object('apbmntxqtvxjzini')
+
+        self.assertRequest(req_info, request_text)
+
+        self.assertEqual(result.content_length, 10)
         self.assertEqual(result.status, 200)
-        self.assertEqual(result.request_id, resp.headers['x-oss-request-id'])
+        self.assertEqual(result.request_id, '566B6BEBD4C05B21E97261B0')
         self.assertEqual(result.object_type, 'Normal')
-        self.assertEqual(result.content_type, resp.headers['Content-Type'])
-        self.assertEqual(result.etag, resp.headers['ETag'].strip('"'))
-        self.assertEqual(result.last_modified, MTIME)
+        self.assertEqual(result.content_type, 'application/octet-stream')
+        self.assertEqual(result.etag, '0CF031A5EB9351746195B20B86FD3F68')
+        self.assertEqual(result.last_modified, 1449880554)
 
     @patch('oss2.Session.do_request')
-    def test_object_exists(self, do_request):
-        do_request.return_value = r4head(0, in_status=304)
-        self.assertTrue(bucket().object_exists('fake-key'))
+    def test_object_exists_true(self, do_request):
+        request_text = '''GET /sbowspxjhmccpmesjqcwagfw HTTP/1.1
+Host: ming-oss-share.oss-cn-hangzhou.aliyuncs.com
+Accept-Encoding: identity
+Connection: keep-alive
+if-modified-since: Sun, 13 Dec 2015 00:37:17 GMT
+date: Sat, 12 Dec 2015 00:37:17 GMT
+User-Agent: aliyun-sdk-python/2.0.2(Windows/7/;3.3.3)
+Accept: */*
+authorization: OSS ZCDmm7TPZKHtx77j:wopWcmMd/70eNKYOc9M6ZA21yY8='''
 
-        body = '''<?xml version="1.0" encoding="UTF-8"?>
-        <Error>
-            <Code>NoSuchKey</Code>
-            <Message>The specified key does not exist.</Message>
-            <RequestId>566B6C3D6086505A0CFF0F68</RequestId>
-            <HostId>fake-bucket.oss-cn-hangzhou.aliyuncs.com</HostId>
-            <Key>fake-key</Key>
-        </Error>'''
-        do_request.auto_spec = True
-        do_request.side_effect = partial(do4body, status=404, body=body, content_type='application/xml')
+        response_text = '''HTTP/1.1 304 Not Modified
+Server: AliyunOSS
+Date: Sat, 12 Dec 2015 00:37:17 GMT
+Content-Type: application/octet-stream
+Connection: keep-alive
+x-oss-request-id: 566B6C3D010B7A4314D2253D
+Accept-Ranges: bytes
+ETag: "5EB63BBBE01EEED093CB22BB8F5ACDC3"
+Last-Modified: Sat, 12 Dec 2015 00:37:17 GMT
+x-oss-object-type: Normal'''
 
-        self.assertTrue(not bucket().object_exists('fake-key'))
+        req_info = mock_response(do_request, response_text)
+
+        self.assertTrue(bucket().object_exists('sbowspxjhmccpmesjqcwagfw'))
+        self.assertRequest(req_info, request_text)
+
+    @patch('oss2.Session.do_request')
+    def test_object_exists_false(self, do_request):
+        request_text = '''GET /sbowspxjhmccpmesjqcwagfw HTTP/1.1
+Host: ming-oss-share.oss-cn-hangzhou.aliyuncs.com
+Accept-Encoding: identity
+Connection: keep-alive
+if-modified-since: Sun, 13 Dec 2015 00:37:17 GMT
+date: Sat, 12 Dec 2015 00:37:17 GMT
+User-Agent: aliyun-sdk-python/2.0.2(Windows/7/;3.3.3)
+Accept: */*
+authorization: OSS ZCDmm7TPZKHtx77j:wopWcmMd/70eNKYOc9M6ZA21yY8='''
+
+        response_text = '''HTTP/1.1 404 Not Found
+Server: AliyunOSS
+Date: Sat, 12 Dec 2015 00:37:17 GMT
+Content-Type: application/xml
+Content-Length: 287
+Connection: keep-alive
+x-oss-request-id: 566B6C3D6086505A0CFF0F68
+
+<?xml version="1.0" encoding="UTF-8"?>
+<Error>
+  <Code>NoSuchKey</Code>
+  <Message>The specified key does not exist.</Message>
+  <RequestId>566B6C3D6086505A0CFF0F68</RequestId>
+  <HostId>ming-oss-share.oss-cn-hangzhou.aliyuncs.com</HostId>
+  <Key>sbowspxjhmccpmesjqcwagfw</Key>
+</Error>'''
+
+        req_info = mock_response(do_request, response_text)
+        self.assertTrue(not bucket().object_exists('sbowspxjhmccpmesjqcwagfw'))
+        self.assertRequest(req_info, request_text)
 
     @patch('oss2.Session.do_request')
     def test_get(self, do_request):
-        size = 1023
-        resp = r4get(random_bytes(size))
-        do_request.return_value = resp
+        content = random_bytes(1023)
 
-        result = bucket().get_object('fake-key')
+        request_text = '''GET /sjbhlsgsbecvlpbf HTTP/1.1
+Host: ming-oss-share.oss-cn-hangzhou.aliyuncs.com
+Accept-Encoding: identity
+Connection: keep-alive
+date: Sat, 12 Dec 2015 00:35:53 GMT
+User-Agent: aliyun-sdk-python/2.0.2(Windows/7/;3.3.3)
+Accept: */*
+authorization: OSS ZCDmm7TPZKHtx77j:PAedG7U86ZxQ2WTB+GdpSltoiTI='''
 
-        self.assertEqual(result.read(), resp.body)
-        self.assertEqual(result.content_length, size)
+        response_text = '''HTTP/1.1 200 OK
+Server: AliyunOSS
+Date: Sat, 12 Dec 2015 00:35:53 GMT
+Content-Type: text/plain
+Content-Length: {0}
+Connection: keep-alive
+x-oss-request-id: 566B6BE93A7B8CFD53D4BAA3
+Accept-Ranges: bytes
+ETag: "D80CF0E5BE2436514894D64B2BCFB2AE"
+Last-Modified: Sat, 12 Dec 2015 00:35:53 GMT
+x-oss-object-type: Normal
+
+{1}'''.format(len(content), to_string(content))
+
+        req_info = mock_response(do_request, response_text)
+
+        result = bucket().get_object('sjbhlsgsbecvlpbf')
+
+        self.assertRequest(req_info, request_text)
+
+        self.assertEqual(result.read(), content)
+        self.assertEqual(result.content_length, len(content))
         self.assertEqual(result.status, 200)
-        self.assertEqual(result.request_id, resp.headers['x-oss-request-id'])
+        self.assertEqual(result.request_id, '566B6BE93A7B8CFD53D4BAA3')
         self.assertEqual(result.object_type, 'Normal')
-        self.assertEqual(result.content_type, resp.headers['Content-Type'])
-        self.assertEqual(result.etag, resp.headers['ETag'].strip('"'))
-        self.assertEqual(result.last_modified, MTIME)
+        self.assertEqual(result.content_type, 'text/plain')
+        self.assertEqual(result.etag, 'D80CF0E5BE2436514894D64B2BCFB2AE')
+        self.assertEqual(result.last_modified, 1449880553)
 
     @patch('oss2.Session.do_request')
     def test_get_with_progress(self, do_request):
-        size = 1024 * 1024 + 1
-        resp = r4get(random_bytes(size))
-        do_request.return_value = resp
+        content = random_bytes(1024 * 1024 + 1)
+
+        request_text = '''GET /sjbhlsgsbecvlpbf HTTP/1.1
+Host: ming-oss-share.oss-cn-hangzhou.aliyuncs.com
+Accept-Encoding: identity
+Connection: keep-alive
+date: Sat, 12 Dec 2015 00:35:53 GMT
+User-Agent: aliyun-sdk-python/2.0.2(Windows/7/;3.3.3)
+Accept: */*
+authorization: OSS ZCDmm7TPZKHtx77j:PAedG7U86ZxQ2WTB+GdpSltoiTI='''
+
+        response_text = '''HTTP/1.1 200 OK
+Server: AliyunOSS
+Date: Sat, 12 Dec 2015 00:35:53 GMT
+Content-Type: text/plain
+Content-Length: {0}
+Connection: keep-alive
+x-oss-request-id: 566B6BE93A7B8CFD53D4BAA3
+Accept-Ranges: bytes
+ETag: "D80CF0E5BE2436514894D64B2BCFB2AE"
+Last-Modified: Sat, 12 Dec 2015 00:35:53 GMT
+x-oss-object-type: Normal
+
+{1}'''.format(len(content), to_string(content))
+
+        req_info = mock_response(do_request, response_text)
 
         self.previous = -1
-        result = bucket().get_object('fake-key', progress_callback=self.progress_callback)
-        content = read_file(result)
+        result = bucket().get_object('sjbhlsgsbecvlpbf', progress_callback=self.progress_callback)
 
-        self.assertEqual(self.previous, size)
-        self.assertEqual(len(content), size)
-        self.assertEqual(content, resp.body)
+        self.assertRequest(req_info, request_text)
+
+        content_read = read_file(result)
+
+        self.assertEqual(self.previous, len(content))
+        self.assertEqual(len(content_read), len(content))
+        self.assertEqual(content_read, content)
 
     @patch('oss2.Session.do_request')
     def test_get_to_file(self, do_request):
