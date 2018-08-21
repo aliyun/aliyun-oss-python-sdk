7c908527d0445b54bbab9fa848404e47065d9d61
wanyuanyang <wanyuan.ywy@alibaba-inc.com>
2018-08-21 16:21:49 +0800

    add unittest for get range

---
+++ unittests/test_utils.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from oss2.utils import *
+
+
+class TestUtils(unittest.TestCase):
+    def test_is_multiple_sizeof_encrypt_block(self):
+        byte_range_start = 1024
+        is_multiple = is_multiple_sizeof_encrypt_block(byte_range_start)
+        self.assertTrue(is_multiple)
+
+        byte_range_start = 1025
+        is_multiple = is_multiple_sizeof_encrypt_block(byte_range_start)
+        self.assertFalse(is_multiple)
+        
+    def test_calc_aes_ctr_offset_by_range(self):
+        byte_range_start = 1024
+        cout_offset = calc_aes_ctr_offset_by_range(byte_range_start)
+        self.assertEqual(cout_offset, 1024 / 16)
