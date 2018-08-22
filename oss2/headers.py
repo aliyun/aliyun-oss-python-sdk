5c5530264888b4ed996a7e4b788aeed41fa9b84a
wanyuanyang <wanyuan.ywy@alibaba-inc.com>
2018-08-22 21:46:36 +0800

    add requestHeader

---
+++ oss2/headers.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+oss2.headers
+~~~~~~~~
+这个模块包含了发送http请求的header, 类型为dict
+"""
+
+OSS_SERVER_SIDE_ENCRYPTION = "x-oss-server-side-encryption"
+OSS_SERVER_SIDE_ENCRYPTION_KEY_ID = "x-oss-server-side-encryption-key-id"
+
+class requestHeader(dict): 
+    def __init__(self, *arg, **kw): 
+        super(requestHeader, self).__init__(*arg, **kw)
+
+    def setServerSideEncryption(self, algorithm=None, cmk_id=None):
+        if algorithm is "AES256":
+            self[OSS_SERVER_SIDE_ENCRYPTION] = "AES256"
+        elif algorithm is "KMS":
+            self[OSS_SERVER_SIDE_ENCRYPTION] = "KMS"
+            if cmk_id is not None:
+                self[OSS_SERVER_SIDE_ENCRYPTION_KEY_ID] = cmk_id
+        else:
+            return
