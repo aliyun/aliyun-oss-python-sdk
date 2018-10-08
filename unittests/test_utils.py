8b56d0d654df299f89bde4b6681074bde7b8f878
wanyuanyang <wanyuan.ywy@alibaba-inc.com>
2018-10-08 20:33:30 +0800

    add utils unittest

--- unittests/test_utils.py
+++ unittests/test_utils.py
@@ -18,3 +18,14 @@ class TestUtils(unittest.TestCase):
         byte_range_start = 1024
         cout_offset = calc_aes_ctr_offset_by_data_offset(byte_range_start)
         self.assertEqual(cout_offset, 1024 / 16)
+
+    def test_is_valid_crypto_part_size(self):
+        self.assertFalse(is_valid_crypto_part_size(1, 1024*1024*100))
+        self.assertFalse(is_valid_crypto_part_size(1024 * 100 + 1, 1024*1024*100))
+        self.assertFalse(is_valid_crypto_part_size(1024 * 100, 1024*1024*1024))
+        self.assertTrue(is_valid_crypto_part_size(1024 * 100, 1024*1024*100))
+
+    def test_determine_crypto_part_size(self):
+        self.assertEqual(determine_crypto_part_size(1024*100*100000), 1024*1000)
+        self.assertEqual(determine_crypto_part_size(1024*100*100000 - 1), 1024112)
+        self.assertEqual(determine_crypto_part_size(1024*100*99), 1024*100)
