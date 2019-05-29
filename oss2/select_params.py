22f305b254033d74cab4323d7813e66c65348803
zhangfeng.dzf <zhangfeng.dzf@alibaba-inc.com>
2019-05-29 21:50:33 +0800

    add encoding

--- oss2/select_params.py
+++ oss2/select_params.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 class SelectParameters(object):  # Select API中参数名常量，参数名是大小写敏感的。具体含义参加api.py中的介绍
     CsvHeaderInfo = 'CsvHeaderInfo'
     CommentCharacter = 'CommentCharacter'
