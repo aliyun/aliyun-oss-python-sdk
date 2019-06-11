a1996022dd288b5a986cd07b2694f5af514296e4
liyanzhang505 <liyanzhang2018@dingtalk.com>
2019-06-11 14:19:07 +0800

    Delete unnecessary annotation in examples/bucket_policy.py

--- examples/bucket_policy.py
+++ examples/bucket_policy.py
@@ -12,7 +12,6 @@ import json
 # 以杭州区域为例，Endpoint可以是：
 #   http://oss-cn-hangzhou.aliyuncs.com
 #   https://oss-cn-hangzhou.aliyuncs.com
-# 分别以HTTP、HTTPS协议访问。
 access_key_id = os.getenv('OSS_TEST_ACCESS_KEY_ID', '<你的AccessKeyId>')
 access_key_secret = os.getenv('OSS_TEST_ACCESS_KEY_SECRET', '<你的AccessKeySecret>')
 bucket_name = os.getenv('OSS_TEST_BUCKET', '<你的Bucket>')
