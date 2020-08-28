9c5da7bf0b6cc3917f2aed33336b039b7f921c40
guangjun.hgj <guangjun.hgj@alibaba-inc.com>
2020-08-28 10:17:04 +0800

    support python3.8

--- tests/test_headers.py
+++ tests/test_headers.py
@@ -9,15 +9,15 @@ class TestHeaders(OssTestCase):
         myHeader = RequestHeader()
         
         myHeader.set_server_side_encryption(algorithm="AES256")
-        self.assertTrue(myHeader["x-oss-server-side-encryption"] is "AES256")
+        self.assertEqual(myHeader["x-oss-server-side-encryption"], "AES256")
 
         myHeader.set_server_side_encryption(algorithm='KMS')
-        self.assertTrue(myHeader["x-oss-server-side-encryption"] is "KMS")
+        self.assertEqual(myHeader["x-oss-server-side-encryption"], "KMS")
         self.assertTrue("x-oss-server-side-encryption-key-id" not in myHeader)
 
         myHeader.set_server_side_encryption(algorithm="KMS", cmk_id="1111")
-        self.assertTrue(myHeader["x-oss-server-side-encryption"] is "KMS")
-        self.assertTrue(myHeader["x-oss-server-side-encryption-key-id"] is "1111")
+        self.assertEqual(myHeader["x-oss-server-side-encryption"], "KMS")
+        self.assertEqual(myHeader["x-oss-server-side-encryption-key-id"], "1111")
 
         myHeader.set_server_side_encryption(algorithm="aaa")
         self.assertTrue("x-oss-server-side-encryption" not in myHeader)
