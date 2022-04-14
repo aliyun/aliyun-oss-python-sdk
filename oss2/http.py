9c24d1bb3ad4156ab31ee71cd38800df6f88d7d5
zxl01071856 <zxl01071856@alibaba-inc.com>
2022-04-14 19:01:39 +0800

    Provide the maximum number of HTTP connections

--- oss2/http.py
+++ oss2/http.py
@@ -29,10 +29,10 @@ logger = logging.getLogger(__name__)
 class Session(object):
     """属于同一个Session的请求共享一组连接池，如有可能也会重用HTTP连接。"""
 
-    def __init__(self):
+    def __init__(self, pool_size=None):
         self.session = requests.Session()
 
-        psize = defaults.connection_pool_size
+        psize = pool_size or defaults.connection_pool_size
         self.session.mount('http://', requests.adapters.HTTPAdapter(pool_connections=psize, pool_maxsize=psize))
         self.session.mount('https://', requests.adapters.HTTPAdapter(pool_connections=psize, pool_maxsize=psize))
 
